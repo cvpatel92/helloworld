4c3c697c5aaf9706d85af799de673d7928bf8475
cvpatel92 <cvpatel92@gmail.com>
2017-03-31 15:35:46 -0400

    Database configuration and Initialize application

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pydj',
+        'USER': 'cvpatel92',
+        'PASSWORD': '$Hreeji6392',
+        'HOST': 'cvpatel92.mysql.pythonanywhere-services.com',
+        'PORT': '',
     }
 }
 
